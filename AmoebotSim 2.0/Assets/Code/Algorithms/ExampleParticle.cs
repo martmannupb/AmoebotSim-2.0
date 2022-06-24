cc51640914e0ca8e6015ca4355e8c36c623e191a
Matthias Artmann <martmann@mail.uni-paderborn.de>
2022-06-24 16:42:26 +0200

    Added ExampleParticle

---
+++ AmoebotSim 2.0/Assets/Code/Algorithms/ExampleParticle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State { IDLE, ROOT, LEADER }
+
+public class ExampleParticle : Particle
+{
+    public ParticleAttribute_Int myInt;
+    public ParticleAttribute_Enum<State> myEnum;
+
+    public ExampleParticle(ParticleSystem system, int x = 0, int y = 0) : base(system, x, y)
+    {
+        myInt = new ParticleAttribute_Int("Display name of myInt", 0);
+        myEnum = new ParticleAttribute_Enum<State>("Display name of myEnum", State.IDLE);
+    }
+
+    public override void Activate()
+    {
+        Debug.Log("myInt: " + myInt.ToString());
+        Debug.Log("myEnum: " + myEnum.ToString());
+        myInt.UpdateParameterValue("42");
+        myEnum.UpdateParameterValue("LEADER");
+        Debug.Log("myInt after: " + myInt.ToString());
+        Debug.Log("myEnum after: " + myEnum.ToString());
+        //Debug.Log("myInt before: " + myInt.ToString());
+        //myInt = 42;
+        //Debug.Log("myInt after assignment: " + myInt.ToString());
+        //myInt++;
+        //Debug.Log("myInt after increment: " + myInt.ToString());
+        //myInt--;
+        //Debug.Log("myInt after decrement: " + myInt.ToString());
+    }
+}
