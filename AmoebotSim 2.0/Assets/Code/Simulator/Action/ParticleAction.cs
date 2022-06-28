be863522c2a30ffb6f9282c85d24c07da4952fa4
Matthias Artmann <martmann@mail.uni-paderborn.de>
2022-06-28 18:04:01 +0200

    Added ParticleAction class to represent (mainly movement) actions that need to be queued

---
+++ AmoebotSim 2.0/Assets/Code/Simulator/Action/ParticleAction.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ActionType { EXPAND, CONTRACT_HEAD, CONTRACT_TAIL, PUSH, PULL_HEAD, PULL_TAIL }
+
+/// <summary>
+/// Represents an action a particle can schedule when it is activated.
+/// <para>
+/// Some particle actions need to be scheduled because applying them
+/// immediately would violate the FSYNC execution model where all particles
+/// operate on the same snapshot of the system. Thus, these actions are
+/// scheduled and only applied after all particles have been activated.
+/// </para>
+/// </summary>
+public class ParticleAction
+{
+    public Particle particle;
+    public ActionType type;
+    public int localDir;
+
+    public ParticleAction(Particle particle, ActionType type, int localDir = -1)
+    {
+        this.particle = particle;
+        this.type = type;
+        this.localDir = localDir;
+    }
+}
