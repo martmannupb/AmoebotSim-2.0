8b303e52c636daad4e28b7b1394163e84609110a
Tobias Maurer <tobias.maurer.it@web.de>
2022-08-29 15:48:37 +0200

    Cleaning Up a little

--- AmoebotSim 2.0/Assets/Code/AmoebotSimulator.cs
+++ AmoebotSim 2.0/Assets/Code/AmoebotSimulator.cs
@@ -36,11 +36,11 @@ public class AmoebotSimulator : MonoBehaviour
         // Init Algorithm
         //system.InitializeExample(1, 1, 1f, -9, -5);
         //system.InitializeExample(50, 50, 0.3f, -9, -5);
-        //system.InitializeLineFormation(50, 0.4f);
+        system.InitializeLineFormation(50, 0.4f);
         //system.InitializeLineFormation(25, 0.4f);
         //system.InitializeLeaderElection(50, 0.35f);
         //system.InitializeChiralityCompass(50, 0.2f);
-        system.InitializeBoundaryTest(100, 0.05f);
+        //system.InitializeBoundaryTest(100, 0.05f);
         //system.InitializeExpandedTest(10);
     }
 
@@ -64,7 +64,6 @@ public class AmoebotSimulator : MonoBehaviour
     {
         //system.ActivateRandomParticle();
         system.SimulateRound();
-        UpdateRoundCounter();
     }
 
     public void SetSimSpeed(float roundTime)
@@ -76,12 +75,6 @@ public class AmoebotSimulator : MonoBehaviour
         }
         Time.fixedDeltaTime = roundTime;
         renderSystem.SetRoundTime(roundTime);
-    }
-
-    private void UpdateRoundCounter()
-    {
-        roundsText.text = "Round: " + system.CurrentRound;
-        maxRoundText.text = "Max: " + system.LatestRound;
     }
 
 
@@ -114,71 +107,5 @@ public class AmoebotSimulator : MonoBehaviour
     public void ToggleCircuits()
     {
         renderSystem.ToggleCircuits();
-    }
-
-    public void ToggleAA()
-    {
-        renderSystem.ToggleAntiAliasing();
-    }
-
-    public void StepBack()
-    {
-        if (!running)
-        {
-            system.StepBack();
-            UpdateRoundCounter();
-        }
-    }
-
-    public void StepForward()
-    {
-        if (!running)
-        {
-            system.StepForward();
-            UpdateRoundCounter();
-        }
-    }
-
-    public void Jump()
-    {
-        if (!running)
-        {
-            string text = roundInput.text;
-            if (int.TryParse(text, out int round))
-            {
-                if (round < system.EarliestRound || round > system.LatestRound)
-                {
-                    Debug.LogWarning("Round must be between earliest round (" + system.EarliestRound + ") and latest round (" + system.LatestRound + ")");
-                }
-                else
-                {
-                    system.SetMarkerToRound(round);
-                    UpdateRoundCounter();
-                }
-            }
-            else
-            {
-                Debug.LogWarning("Must enter a number");
-            }
-        }
-    }
-
-    public void CutOff()
-    {
-        if (!running)
-        {
-            system.CutOffAtMarker();
-            UpdateRoundCounter();
-        }
-    }
-
-    public void ResetSystem()
-    {
-        if (!running)
-        {
-            system.Reset();
-            system.InitializeLineFormation(50, 0.4f);
-            UpdateRoundCounter();
-        }
     }
 }
