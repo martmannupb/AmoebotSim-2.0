889ad0bda81ade7cc545ee13f67a8a7b6e09cb06
Matthias Artmann <martmann@mail.uni-paderborn.de>
2022-09-28 11:30:18 +0200

    Added convenience methods to ParticleAction

--- AmoebotSim 2.0/Assets/Code/Simulator/Action/ParticleAction.cs
+++ AmoebotSim 2.0/Assets/Code/Simulator/Action/ParticleAction.cs
@@ -22,4 +22,84 @@ public class ParticleAction
         this.type = type;
         this.localDir = localDir;
     }
+
+    /// <summary>
+    /// Checks whether this action is a contraction of any kind.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents a regular
+    /// contraction or a handover contraction.</returns>
+    public bool IsContraction()
+    {
+        return type == ActionType.CONTRACT_HEAD || type == ActionType.CONTRACT_TAIL || type == ActionType.PULL_HEAD || type == ActionType.PULL_TAIL;
+    }
+
+    /// <summary>
+    /// Checks whether this action is a regular contraction.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents a
+    /// regular contraction, not a handover.</returns>
+    public bool IsRegularContraction()
+    {
+        return type == ActionType.CONTRACT_HEAD || type == ActionType.CONTRACT_TAIL;
+    }
+
+    /// <summary>
+    /// Checks whether this action is a handover contraction.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents a handover
+    /// contraction and not a regular contraction.</returns>
+    public bool IsHandoverContraction()
+    {
+        return type == ActionType.PULL_HEAD || type == ActionType.PULL_TAIL;
+    }
+
+    /// <summary>
+    /// Checks whether this action is an expansion of any kind.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents a
+    /// regular expansion or a handover expansion.</returns>
+    public bool IsExpansion()
+    {
+        return type == ActionType.EXPAND || type == ActionType.PUSH;
+    }
+
+    /// <summary>
+    /// Checks whether this action is a regular expansion.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents a
+    /// regular expansion, not a handover.</returns>
+    public bool IsRegularExpansion()
+    {
+        return type == ActionType.EXPAND;
+    }
+
+    /// <summary>
+    /// Checks whether this action is a handover expansion.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents a
+    /// handover expansion, not a regular expansion.</returns>
+    public bool IsHandoverExpansion()
+    {
+        return type == ActionType.PUSH;
+    }
+
+    /// <summary>
+    /// Checks whether this action is a handover.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents
+    /// a handover contraction or expansion and not a regular movement.</returns>
+    public bool IsHandover()
+    {
+        return type == ActionType.PUSH || type == ActionType.PULL_HEAD || type == ActionType.PULL_TAIL;
+    }
+
+    /// <summary>
+    /// Checks whether this action is a regular movement.
+    /// </summary>
+    /// <returns><c>true</c> if and only if the action represents a regular
+    /// contraction or expansion and not a handover movement.</returns>
+    public bool IsRegularMovement()
+    {
+        return type == ActionType.EXPAND || type == ActionType.CONTRACT_HEAD || type == ActionType.CONTRACT_TAIL;
+    }
 }
