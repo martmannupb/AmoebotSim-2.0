43f1d560642fa407fa855cf461e734b3f1b22c69
Tobias Maurer <tobias.maurer.it@web.de>
2022-10-03 16:06:13 +0200

    Editing some prefabs and planning the next feature

--- AmoebotSim 2.0/Assets/Code/Graphics/UI/UIHandler.cs
+++ AmoebotSim 2.0/Assets/Code/Graphics/UI/UIHandler.cs
@@ -462,8 +462,7 @@ public class UIHandler : MonoBehaviour
     {
         Vector2 pos;
         if (sim.system.particles.Count <= 1000) pos = sim.system.BBoxCenterPosition();
-        else pos = sim.system.SeedPosition();
-        pos = AmoebotFunctions.CalculateAmoebotCenterPositionVector2((int)pos.x, (int)pos.y);
+        else pos = AmoebotFunctions.CalculateAmoebotCenterPositionVector2(sim.system.SeedPosition());
         Camera.main.transform.position = new Vector3(pos.x, pos.y, Camera.main.transform.position.z);
     }
 
