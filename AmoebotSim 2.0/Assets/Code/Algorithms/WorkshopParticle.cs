4188c3dfb7b5d9df2053388ea26edc2ca3b35d32
Matthias Artmann <martmann@mail.uni-paderborn.de>
2022-11-01 15:18:10 +0100

    Created basic algorithm as workshop template

---
+++ AmoebotSim 2.0/Assets/Code/Algorithms/WorkshopParticle.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class WorkshopParticle : ParticleAlgorithm
+{
+    // Specify the number of pins (may be 0)
+    public override int PinsPerEdge => 1;
+
+    // This is the display name of the algorithm (must be unique)
+    public static new string Name => "Workshop";
+
+    // If the algorithm has a special generation method, specify its full name here
+    public static new string GenerationMethod => typeof(WorkshopParticleInitializer).FullName;
+
+    // Declare attributes here
+    // ...
+
+    public WorkshopParticle(Particle p) : base(p)
+    {
+        // Initialize the attributes here
+        // Also, set the default initial color
+        //SetMainColor(ColorData.Particle_Black);
+    }
+
+    // Implement this if the particles require special initialization
+    // The parameters will be converted to particle attributes for initialization
+    public void Init(bool leader = false)
+    {
+        // This code is executed directly after the constructor
+        if (leader)
+            SetMainColor(ColorData.Particle_Blue);
+    }
+
+    // Implement this method if the algorithm terminates at some point
+    //public override bool IsFinished()
+    //{
+    //    // Return true when this particle has terminated
+    //    return false;
+    //}
+
+    // The movement activation method
+    public override void ActivateMove()
+    {
+        // Implement the movement code here
+    }
+
+    // The beep activation method
+    public override void ActivateBeep()
+    {
+        // Implement the communication code here
+    }
+}
+
+// Use this to implement a generation method for this algorithm
+// Its class name must be specified as the algorithm's GenerationMethod
+public class WorkshopParticleInitializer : InitializationMethod
+{
+    public WorkshopParticleInitializer(ParticleSystem system) : base(system) { }
+
+    // This method implements the system generation
+    // Its parameters will be shown in the UI and they must have default values
+    public void Generate(/* Parameters with default values */)
+    {
+        PlaceParallelogram(Vector2Int.zero, Direction.E, 10);
+        // Select a random leader
+        InitializationParticle[] particles = GetParticles();
+        particles[Random.Range(0, particles.Length)].SetAttribute("leader", true);
+    }
+}
